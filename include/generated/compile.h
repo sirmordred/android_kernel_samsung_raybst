cc3da01b6cc63533c4c842534b72fe1d3352bf30
rbheromax <rbheromax@gmail.com>
2013-11-30 09:34:35 -0600

    make mrproper

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Thu Sep 19 14:03:47 CDT 2013"
-#define LINUX_COMPILE_BY "rbheromax"
-#define LINUX_COMPILE_HOST "agerthorn"
-#define LINUX_COMPILER "gcc version 4.7.3 20121106 (prerelease) (Linaro GCC 4.7-2012.11) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.4.0-rb3-v5.2.1-g89906b5-dirty"
